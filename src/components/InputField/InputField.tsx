3442c096d5447a290d2673d6e1faa29edb629473
Christian Pilegaard Cederquist <christian.cederquist@gmail.com>
2025-05-08 09:34:49 +0200

    Finished input field

--- src/components/InputField/InputField.tsx
+++ src/components/InputField/InputField.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { mergeClassnames } from "../../util/merge-classnames";
 
 export type InputFieldProps = {
@@ -12,15 +12,26 @@ export type InputFieldProps = {
   & ({ inputCharWidth: '4' | '8' | '11' | '27' } | { inputWidth: 'xxs' | 'xs' | 's' | 'm' | 'l' | 'xl' })
 export function InputField({ labelProps, inputProps, formGroupProps, hint, characterLimit, ...props }: Readonly<InputFieldProps>) {
   const [inputCount, setInputCount] = useState<number>(typeof inputProps.value === 'number' ? inputProps.value : inputProps.value?.length ?? 0);
-  const charactersLeft = (characterLimit ?? 0) - (inputCount ?? 0)
+  const [lastInputEventMs, setLastInputEventMs] = useState<number | undefined>(undefined);
+  const [visibleInputCount, setVisibleInputCount] = useState<number>(inputCount);
+  const charactersLeft = (characterLimit ?? 0) - (visibleInputCount ?? 0)
   const input = <input
     className={mergeClassnames("form-input", 'inputCharWidth' in props && `input-char-${props.inputCharWidth}`, 'inputWidth' in props && `input-width-${props.inputWidth}`)}
+    aria-describedby={characterLimit !== undefined ? `${inputProps.id}-limit-message` : undefined}
     {...inputProps}
     onChange={characterLimit !== undefined ? (ev) => {
-      setInputCount(ev.target.value.length); // TODO: Debounce this by 500ms
+      setInputCount(ev.target.value.length);
+      setLastInputEventMs(Date.now());
       inputProps.onChange?.(ev)
     } : inputProps.onChange}
   />;
+  useEffect(() => {
+    setInterval(() => {
+      if (lastInputEventMs === undefined || lastInputEventMs < Date.now() - 500) {
+        setVisibleInputCount(inputCount);
+      }
+    }, 1000);
+  }, [])
   return (
     <div className="form-group" {...formGroupProps}>
       <label className="form-label" htmlFor={inputProps.id} {...labelProps}></label>
@@ -29,7 +40,7 @@ export function InputField({ labelProps, inputProps, formGroupProps, hint, chara
       {'suffix' in props && <div className="form-input-wrapper form-input-wrapper--suffix">{input}<div className="form-input-suffix" aria-hidden="true"></div></div>}
       {!('suffix' in props) && !('prefix' in props) && input}
       {characterLimit && <>
-        <span id="input-type-text-limit-limit-message" className="sr-only">Du kan indtaste op til {charactersLeft}
+        <span id={inputProps.id+"-limit-message"} className="sr-only">Du kan indtaste op til {charactersLeft}
         tegn</span>
         <span className="form-hint character-limit" aria-hidden="true">Du har {charactersLeft} tegn tilbage</span>
         <span className="character-limit-sr-only sr-only" aria-live="polite">Du har {charactersLeft} tegn
