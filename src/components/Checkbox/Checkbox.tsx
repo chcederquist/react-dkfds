939d52cd32056ed8ee7242bd3d2e638a7624381f
Christian Pilegaard Cederquist <christian.cederquist@gmail.com>
2025-05-09 23:39:08 +0200

    Checkbox component added

---
+++ src/components/Checkbox/Checkbox.tsx
@@ -0,0 +1,32 @@
+import { mergeStrings } from "../../util/merge-classnames";
+
+export type CheckboxProps = {
+  legendProps: React.DetailedHTMLProps<React.HTMLAttributes<HTMLLegendElement>, HTMLLegendElement>;
+  fieldsetProps: React.DetailedHTMLProps<React.FieldsetHTMLAttributes<HTMLFieldSetElement>, HTMLFieldSetElement>;
+  values: {value: string; id: string; label: string, hint?: string;}[];
+  id: string;
+  hint?: string;
+  errorMessage?: string;
+  name: string;
+}
+
+export function Checkbox({ fieldsetProps, hint, id, legendProps, errorMessage, values, name }: Readonly<CheckboxProps>) {
+  return (
+    <div className="form-group">
+    <fieldset aria-describedby={mergeStrings(hint && id+'-hint', errorMessage && id+'-error')} {...fieldsetProps}>
+
+        <legend className="form-label" {...legendProps}></legend>
+        {hint && <span className="form-hint" id={id+'-hint'}>{hint}</span>}
+        {errorMessage && <span className="form-error-message" id={id+'-error'}><span className="sr-only">Fejl: </span>{errorMessage}</span>}
+        {values && values.map((value) => (
+          <div className="form-group-radio" key={value.id}>
+            <input type="checkbox" id={value.id} name={name}
+                className="form-checkbox" value={value.value} />
+            <label className="form-label" htmlFor={value.id}>{value.label}</label>
+            {hint && <span className="form-hint" id={value.id + "-hint"}>{hint}</span>}
+          </div>
+        ))}
+    </fieldset>
+</div>
+  )
+}
