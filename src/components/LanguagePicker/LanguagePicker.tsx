5accf2f0b7e65e9749006fecd83806eca524d881
Christian Pilegaard Cederquist <christian.cederquist@gmail.com>
2025-05-10 21:49:57 +0200

    Updated eslint config and ran prettier on languagepicker

--- src/components/LanguagePicker/LanguagePicker.tsx
+++ src/components/LanguagePicker/LanguagePicker.tsx
@@ -1,18 +1,38 @@
-export type LanguagePickerProps = { selectLanguageAriaLabel?: string; languages: {isCurrentLanguage: boolean; languageSelectedLabel: string; languageCode: string; label: string}[] }
+export type LanguagePickerProps = {
+  selectLanguageAriaLabel?: string;
+  languages: {
+    isCurrentLanguage: boolean;
+    languageSelectedLabel: string;
+    languageCode: string;
+    label: string;
+  }[];
+};
 
-export function LanguagePicker({selectLanguageAriaLabel, languages}: Readonly<LanguagePickerProps>) {
+export function LanguagePicker({
+  selectLanguageAriaLabel,
+  languages,
+}: Readonly<LanguagePickerProps>) {
   return (
     <div className="language-switcher">
       <div className="container">
         <ul aria-label={selectLanguageAriaLabel}>
           {languages.map((language) => (
             <li key={language.languageCode}>
-              <a lang={language.languageCode} aria-label={language.isCurrentLanguage ? language.languageSelectedLabel : undefined} href={`?lang=${language.languageCode}`}>
+              <a
+                lang={language.languageCode}
+                aria-label={
+                  language.isCurrentLanguage
+                    ? language.languageSelectedLabel
+                    : undefined
+                }
+                href={`?lang=${language.languageCode}`}
+              >
                 {language.label}
               </a>
-            </li>))}
+            </li>
+          ))}
         </ul>
       </div>
     </div>
-  )
+  );
 }
