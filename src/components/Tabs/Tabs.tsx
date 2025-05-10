bd0579e00a6dda4a36179a7afa4db6f1be007703
Christian Pilegaard Cederquist <christian.cederquist@gmail.com>
2025-05-10 20:16:58 +0200

    Aligned tabs with DKFDS JS

--- src/components/Tabs/Tabs.tsx
+++ src/components/Tabs/Tabs.tsx
@@ -5,19 +5,38 @@ export type TabsProps = {
 }
 
 export function Tabs({tabs}: Readonly<TabsProps>) {
-  const [activeTab, setActiveTab] = useState(tabs[0].id);
+  const [activeTabIndex, setActiveTabIndex] = useState(0);
   return (
 <div className="tab-container">
   <div className="tab-list">
-  {tabs.map((tab) => {
+  {tabs.map((tab, index) => {
     return (
-      <button className="tab-button" onClick={() => setActiveTab(tab.id)} role="tab" aria-controls={tab.id.toString()} aria-selected={activeTab === tab.id} id={`tabcontrol${tab.id}`}>{tab.title}</button>
+      <button className="tab-button" onKeyDown={(ev) => {
+        ev.preventDefault();
+        if (ev.repeat) {
+          return;
+        }
+        switch (ev.key) {
+          case "ArrowRight":
+            setActiveTabIndex((index + 1) % tabs.length);
+            break;
+          case "ArrowLeft":
+            setActiveTabIndex((index - 1 + tabs.length) % tabs.length);
+            break;
+          case "Home":
+            setActiveTabIndex(0);
+            break;
+          case "End":
+            setActiveTabIndex(tabs.length - 1);
+            break;
+        }
+      }} onClick={() => setActiveTabIndex(index)} role="tab" aria-controls={tab.id.toString()} aria-selected={activeTabIndex === tab.id} id={`tabcontrol${tab.id}`} tabIndex={activeTabIndex === index ? 0 : -1}>{tab.title}</button>
     );
   })}
   </div>
-  {tabs.map((tab) => {
+  {tabs.map((tab, index) => {
     return (
-      <div className="tab-panel" role="tabpanel" tabIndex={0} id={`tabpanel${tab.id}`} aria-labelledby={`tabcontrol${tab.id}`} hidden={activeTab !== tab.id}>
+      <div className="tab-panel" role="tabpanel" tabIndex={0} id={`tabpanel${tab.id}`} aria-labelledby={`tabcontrol${tab.id}`} hidden={activeTabIndex !== index}>
         {tab.content}
     </div>
     );
